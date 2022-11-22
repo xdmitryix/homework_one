b063822c851bb6803044bcf60b8dfb9392fd5a98
dmitry <rbdeman@yandex.ru>
2022-11-22 11:05:42 +0300

    сделал первую задачу

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("введи первое число: ");
+int number_one = int.Parse(Console.ReadLine());
+Console.Write("введи второе число: ");
+int number_two = int.Parse(Console.ReadLine());
+int max = 0;
+if (number_one>number_two)
+{
+    max = number_one;
+}
+else
+{
+    max = number_two;
+}
+Console.WriteLine($"максимальное число: {max}");
